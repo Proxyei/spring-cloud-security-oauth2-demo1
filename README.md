8fdc586632ad1b685922faf9a28a385926a85a14
winfuture <insoul_net@qq.com>
2019-11-14 19:13:12 +0800

    初始化工程，子工程:uaa,resources

---
+++ README.md
@@ -0,0 +1,6 @@
+# 开发环境
+- JDK8 64bit
+- sts 3.9.9
+- maven 3.3.9
+# 功能
+测试分布式的seucurity token
---
+++ spring-cloud-security-oauth2-demo1-uaa/src/main/java/org/spring/cloud/security/oauth2/demo1/uaa/App.java
@@ -0,0 +1,13 @@
+package org.spring.cloud.security.oauth2.demo1.uaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
